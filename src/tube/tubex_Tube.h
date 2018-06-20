6fa2e7fbb4829778e0e0d49c1d7db0f8fbca264a
SimonRohou <simon.rohou@gmail.com>
2018-06-20 15:10:50 +0200

    [src] added empty Tube class

---
+++ src/tube/tubex_Tube.h
@@ -0,0 +1,26 @@
+/* ============================================================================
+ *  tubex-lib - Tube class
+ * ============================================================================
+ *  Copyright : Copyright 2017 Simon Rohou
+ *  License   : This program is distributed under the terms of
+ *              the GNU Lesser General Public License (LGPL).
+ *
+ *  Author(s) : Simon Rohou
+ *  Bug fixes : -
+ *  Created   : 2015
+ * ---------------------------------------------------------------------------- */
+
+#ifndef Tube_HEADER
+#define Tube_HEADER
+
+#include "tubex_TubeVector.h"
+
+namespace tubex
+{
+  class Tube : private TubeVector
+  {
+    
+  };
+}
+
+#endif
