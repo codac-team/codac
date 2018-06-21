8ad510990796651e3b7f25c7042f7b8d498a484d
SimonRohou <simon.rohou@gmail.com>
2018-06-21 15:06:38 +0200

    [tests] restoring tests_picard

--- tests/tests_ctc_picard.cpp
+++ tests/tests_ctc_picard.cpp
@@ -11,33 +11,18 @@ TEST_CASE("CtcPicard")
 {
   SECTION("Test CtcPicard - dim 1")
   {
-    // Scalar functions
+    // TubeSlice functions
 
       Interval domain(0.,0.1);
-      TubeSlice x(domain);
-      x.setInputGate(Interval(1.));
+      TubeSlice x(domain, 1);
+      x.setInputGate(IntervalVector(1, Interval(1.)));
 
       Function f("x", "x");
-      CtcPicard tube_picard;
+      CtcPicard ctc_picard;
 
-      CHECK(x.codomain() == Interval::ALL_REALS);
-      tube_picard.contract(f, x);
-      CHECK(x.codomain().is_superset(Interval(exp(domain))));
-      CHECK(x.outputGate().is_superset(Interval(exp(domain.ub()))));
-
-    // Vector functions
-
-      TubeSlice x2(domain);
-      x2.setInputGate(Interval(1.));
-
-      vector<TubeSlice*> v_x_ptr;
-      Variable var_x;
-      v_x_ptr.push_back(&x2);
-      Function f_vec(var_x, var_x);
-
-      CHECK(x2.codomain() == Interval::ALL_REALS);
-      tube_picard.contract(f_vec, v_x_ptr);
-      CHECK(x2.codomain().is_superset(Interval(exp(domain))));
-      CHECK(x2.outputGate().is_superset(Interval(exp(domain.ub()))));
+      CHECK(x.codomain()[0] == Interval::ALL_REALS);
+      ctc_picard.contract(f, x);
+      CHECK(x.codomain()[0].is_superset(Interval(exp(domain))));
+      CHECK(x.outputGate()[0].is_superset(Interval(exp(domain.ub()))));
   }
 }
