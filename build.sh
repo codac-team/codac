f10df575c7c3ba26d651d3fc2d7b561ad6ad4c03
SimonRohou <simon.rohou@gmail.com>
2018-06-27 15:21:19 +0200

    [travis] using last version of Ibex

--- build.sh
+++ build.sh
@@ -6,12 +6,13 @@
 
 TUBELIB_DIR="$(pwd)"
 
-cd ..
-sh $TUBELIB_DIR/scripts/ibex/build_IbexLib.sh
-cd $TUBELIB_DIR
+#cd ..
+#sh $TUBELIB_DIR/scripts/ibex/build_IbexLib.sh
+#cd $TUBELIB_DIR
 
 mkdir make -p
 cd make
-cmake -DCMAKE_INSTALL_PREFIX=~/ -DIBEX_ROOT=~/ibex -DCMAKE_CXX_COMPILER=g++-5 ..
+#cmake -DCMAKE_INSTALL_PREFIX=~/ -DIBEX_ROOT=~/ibex -DCMAKE_CXX_COMPILER=g++-5 ..
+cmake -DCMAKE_INSTALL_PREFIX=~/ -DCMAKE_CXX_COMPILER=g++-5 ..
 make
 cd ..
--- scripts/ibex/build_IbexLib.sh
+++ scripts/ibex/build_IbexLib.sh
@@ -4,16 +4,23 @@
 
 #!/bin/bash
 
-#set -x # debugging
+echo 'Installing the IBEX-lib...';
+git clone https://github.com/ibex-team/ibex-lib
+cd ibex-lib
+./waf configure --with-debug --interval-lib=filib
+sudo ./waf install
+cd ..
 
-if [ ! -e "$HOME/ibex/lib/libibex.a" ]; then
-  echo 'Installing the IBEX-lib...';
-  git clone https://github.com/benEnsta/ibex-lib.git
-  cd ibex-lib
-  git checkout pyIbex_version_3
-  mkdir build
-  cd build
-  cmake -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${HOME}/ibex ../
-  make -j4
-  make install
-fi
+##set -x # debugging
+#
+#if [ ! -e "$HOME/ibex/lib/libibex.a" ]; then
+#  echo 'Installing the IBEX-lib...';
+#  git clone https://github.com/benEnsta/ibex-lib.git
+#  cd ibex-lib
+#  git checkout pyIbex_version_3
+#  mkdir build
+#  cd build
+#  cmake -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${HOME}/ibex ../
+#  make -j4
+#  make install
+#fi
