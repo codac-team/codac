300c4bef5f2cead02f9e93f7fa73456d6e498f1b
SimonRohou <simon.rohou@gmail.com>
2018-06-27 14:29:20 +0200

    [arithm] cleaning

--- src/arithmetic/old_tubex_operators_unary.h
+++
@@ -1,5 +0,0 @@
-#define unary_op_h(f) \
-  \
-  Tube f(const Tube& x1); \
-  TubeSlice f(const TubeSlice& x1);
-  
--- src/arithmetic/old_tubex_operators_unary_param.h
+++
@@ -1,4 +0,0 @@
-#define unary_param_op_h(f, p) \
-  \
-  Tube f(const Tube& x1, p param); \
-  TubeSlice f(const TubeSlice& x1, p param);
