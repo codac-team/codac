d30bde71c3d65db8d2f62a05ff61fd42885ee6ae
SimonRohou <simon.rohou@gmail.com>
2018-07-20 13:10:40 +0200

    [rob] added new robotics lib

---
+++ src/robotics/data/tubex_DataLoader.cpp
@@ -0,0 +1,28 @@
+/* ============================================================================
+ *  tubex-lib - DataLoader class
+ * ============================================================================
+ *  Copyright : Copyright 2017 Simon Rohou
+ *  License   : This program is distributed under the terms of
+ *              the GNU Lesser General Public License (LGPL).
+ *
+ *  Author(s) : Simon Rohou
+ *  Bug fixes : -
+ *  Created   : 2018
+ * ---------------------------------------------------------------------------- */
+
+#include "tubex_DataLoader.h"
+
+using namespace std;
+
+namespace tubex
+{
+  DataLoader::DataLoader()
+  {
+
+  }
+
+  DataLoader::~DataLoader()
+  {
+
+  }
+}
---
+++ src/robotics/data/tubex_DataLoader.h
@@ -0,0 +1,30 @@
+/* ============================================================================
+ *  tubex-lib - DataLoader class
+ * ============================================================================
+ *  Copyright : Copyright 2017 Simon Rohou
+ *  License   : This program is distributed under the terms of
+ *              the GNU Lesser General Public License (LGPL).
+ *
+ *  Author(s) : Simon Rohou
+ *  Bug fixes : -
+ *  Created   : 2018
+ * ---------------------------------------------------------------------------- */
+
+#ifndef __TUBEX_DATALOADER_H__
+#define __TUBEX_DATALOADER_H__
+
+namespace tubex
+{
+  class DataLoader
+  {
+    public:
+
+      DataLoader();
+      ~DataLoader();
+
+    protected:
+
+  };
+}
+
+#endif
