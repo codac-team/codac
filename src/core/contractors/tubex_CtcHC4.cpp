7bff05cca645e9fe240c584a80643b8747fb45c2
SimonRohou <simon.rohou@gmail.com>
2018-08-23 16:29:11 +0200

    [ctc] faster CtcHC4

--- src/core/contractors/tubex_CtcHC4.cpp
+++ src/core/contractors/tubex_CtcHC4.cpp
@@ -31,38 +31,50 @@ namespace tubex
     if(x.is_empty())
       return false;
 
-    for(int k = 0 ; k < x.nb_slices() ; k++)
+    Slice **v_x_slices = new Slice*[x.size()];
+    for(int i = 0 ; i < x.size() ; i++)
+      v_x_slices[i] = x[i].get_first_slice();
+
+    IntervalVector envelope(x.size() + 1);
+    IntervalVector ingate(x.size() + 1);
+
+    while(v_x_slices[0] != NULL)
     {
-      IntervalVector envelope(x.size() + 1);
-      IntervalVector ingate(x.size() + 1);
-      IntervalVector outgate(x.size() + 1);
+      envelope[0] = v_x_slices[0]->domain();
+      ingate[0] = v_x_slices[0]->domain().lb();
 
-      Slice *slice;
-
-      for(int i = 0 ; i < x.size() ; i++) // accessing values
+      for(int i = 0 ; i < x.size() ; i++)
       {
-        slice = x[i].get_slice(k);
-        envelope[i+1] = slice->codomain();
-        ingate[i+1] = slice->input_gate();
-        outgate[i+1] = slice->output_gate();
+        envelope[i+1] = v_x_slices[i]->codomain();
+        ingate[i+1] = v_x_slices[i]->input_gate();
       }
-      
-      envelope[0] = slice->domain();
-      ingate[0] = slice->domain().lb();
-      outgate[0] = slice->domain().ub();
 
       hc4.contract(envelope);
       hc4.contract(ingate);
-      hc4.contract(outgate);
 
-      for(int i = 0 ; i < x.size() ; i++) // updating values
+      for(int i = 0 ; i < x.size() ; i++)
       {
-        slice = x[i].get_slice(k);
-        slice->set_envelope(envelope[i+1]);
-        slice->set_input_gate(ingate[i+1]);
-        slice->set_output_gate(outgate[i+1]);
+        v_x_slices[i]->set_envelope(envelope[i+1]);
+        v_x_slices[i]->set_input_gate(ingate[i+1]);
       }
+      
+      for(int i = 0 ; i < x.size() ; i++)
+        v_x_slices[i] = v_x_slices[i]->next_slice();
     }
+
+    IntervalVector outgate(x.size() + 1);
+
+    for(int i = 0 ; i < x.size() ; i++)
+      v_x_slices[i] = x[i].get_last_slice();
+
+    outgate[0] = v_x_slices[0]->domain().ub();
+    for(int i = 0 ; i < x.size() ; i++)
+      outgate[i+1] = v_x_slices[i]->output_gate();
+
+    hc4.contract(outgate);
+
+    for(int i = 0 ; i < x.size() ; i++)
+      v_x_slices[i]->set_output_gate(outgate[i+1]);
 
     // todo: return value
   }
