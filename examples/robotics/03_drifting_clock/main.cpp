081ae9ce19fed9aaaf64cefbc3df7b6854e00eae
SimonRohou <simon.rohou@gmail.com>
2018-08-24 16:35:42 +0200

    [rob/ex] restoring drifting clock example

--- examples/robotics/03_drifting_clock/main.cpp
+++ examples/robotics/03_drifting_clock/main.cpp
@@ -29,37 +29,48 @@ int main()
 
   /* =========== INITIALIZATION =========== */
 
-    Tube x1(domain, tube_dt, tubex::Function("100*cos(t)+[80,100]"));
-    Tube x2(domain, tube_dt, tubex::Function("100*sin(t)+[10,30]"));
-    Tube x1dot(domain, tube_dt, tubex::Function("-100*sin(t)+[-0.1,0.1]"));
-    Tube x2dot(domain, tube_dt, tubex::Function("100*cos(t)+[-0.1,0.1]"));
-    Tube y = sqrt(sqr(x1) + sqr(x2) + sqr(beacon_depth));
-    Tube ydot = (x1*x1dot + x2*x2dot) / y;
-    Tube hdot(domain, tube_dt, tubex::Function("[0.08,0.12]*t+[0.97,1.08]"));
-    Tube h = hdot.primitive();
+    TubeVector x(domain, 4);
+    x[0] = Tube(domain, tube_dt, tubex::Function("100*cos(t)+[80,100]"));
+    x[1] = Tube(domain, tube_dt, tubex::Function("100*sin(t)+[10,30]"));
+    x[2] = Tube(domain, tube_dt, tubex::Function("-100*sin(t)+[-0.1,0.1]"));
+    x[3] = Tube(domain, tube_dt, tubex::Function("100*cos(t)+[-0.1,0.1]"));
+    
+    TubeVector y(domain, 2);
+    y[0] = sqrt(sqr(x[0]) + sqr(x[1]) + sqr(beacon_depth));
+    y[1] = (x[0]*x[2] + x[1]*x[3]) / y[0];
+    
+    TubeVector h(domain, 2);
+    h[1] = Tube(domain, tube_dt, tubex::Function("[0.08,0.12]*t+[0.97,1.08]"));
+    h[0] = h[1].primitive();
 
   /* =========== GRAPHICS =========== */
 
     Trajectory y_truth(domain, tubex::Function("sqrt((90+100*cos(t))^2 + (20+100*sin(t))^2 + 100)"));
     Trajectory h_truth(domain, tubex::Function("0.045*t^2+0.98*t"));
 
-    VibesFigure_Tube::draw("Tube [y](·)", &y, &y_truth, 100, 100);
-    VibesFigure_Tube::draw("Tube [h](·)", &h, &h_truth, 150, 150);
+    vibes::beginDrawing();
+
+    VibesFigure_Tube fig_y("Tube [y](·)", &y[0], &y_truth);
+    fig_y.set_properties(100, 100, 500, 250);
+    fig_y.show();
+
+    VibesFigure_Tube fig_h("Tube [h](·)", &h[0], &h_truth);
+    fig_h.set_properties(150, 150, 500, 250);
+    fig_h.show();
 
   /* =========== MEASUREMENTS =========== */
 
-    vector<Interval> v_tau;
-    vector<Interval> v_t;
-    vector<Interval> v_z;
-
-    v_tau.push_back(1.57);  v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(152.47, 156.47));
-    v_tau.push_back(3.34);  v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(34.67, 38.67));
-    v_tau.push_back(5.32);  v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(102.38, 106.38));
-    v_tau.push_back(7.50);  v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(184.45, 188.45));
-    v_tau.push_back(9.88);  v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(167.09, 171.09));
-    v_tau.push_back(12.46); v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(60.03, 64.03));
-    v_tau.push_back(15.25); v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(78.76, 82.76));
-    v_tau.push_back(18.24); v_t.push_back(Interval::ALL_REALS); v_z.push_back(Interval(175.88, 179.88));
+    IntervalVector obs(3);
+    vector<IntervalVector> v_obs; // (tau, t, z)
+    obs[0] = 1.57;  obs[2] = Interval(152.47, 156.47); v_obs.push_back(obs);
+    obs[0] = 3.34;  obs[2] = Interval(34.67, 38.67); v_obs.push_back(obs);
+    obs[0] = 5.32;  obs[2] = Interval(102.38, 106.38); v_obs.push_back(obs);
+    obs[0] = 7.50;  obs[2] = Interval(184.45, 188.45); v_obs.push_back(obs);
+    obs[0] = 9.88;  obs[2] = Interval(167.09, 171.09); v_obs.push_back(obs);
+    obs[0] = 12.46; obs[2] = Interval(60.03, 64.03); v_obs.push_back(obs);
+    obs[0] = 15.25; obs[2] = Interval(78.76, 82.76); v_obs.push_back(obs);
+    obs[0] = 18.24; obs[2] = Interval(175.88, 179.88); v_obs.push_back(obs);
+    // nb: obs[1] set to ALL_REALS by default
   
   /* =========== CONTRACTIONS =========== */
 
@@ -71,24 +82,24 @@ int main()
       cout << "Computation step " << k << "..." << endl;
       contraction = false;
 
-      for(int i = 0 ; i < v_tau.size() ; i++)
+      for(int i = 0 ; i < v_obs.size() ; i++)
       {
-        v_t[i] &= h.invert(v_tau[i]);
-        contraction |= ctc_eval.contract(v_t[i], v_z[i], y, ydot);
-        contraction |= ctc_eval.contract(v_t[i], v_tau[i], h, hdot);
+        contraction |= ctc_eval.contract(v_obs[i][1], v_obs[i][0], h[0], h[1]);
+        contraction |= ctc_eval.contract(v_obs[i][1], v_obs[i][2], y[0], y[1]);
+        contraction |= ctc_eval.contract(v_obs[i][1], v_obs[i][0], h[0], h[1]);
       }
-      
-      contraction |= h.ctc_deriv(hdot);
+
+      contraction |= h[0].ctc_deriv(h[1]);
+      contraction |= y[0].ctc_deriv(y[1]);
       k++;
     } while(contraction);
 
   /* =========== GRAPHICS =========== */
 
-    VibesFigure_Tube::draw("Tube [y](·)");
-    VibesFigure_Tube::draw("Tube [h](·)");
-    VibesFigure_Tube::end_drawing();
+    fig_y.show();
+    fig_h.show();
+    vibes::endDrawing();
 
   // Checking if this example is still working:
-  return (fabs(h.volume() - 9.86763) < 1e-2
-       && fabs(y.volume() - 323.63) < 1e-2) ? EXIT_SUCCESS : EXIT_FAILURE;
+  return (y[0].encloses(y_truth) && h[0].encloses(h_truth)) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
