2dcc01827507531b7301993e0ff664a360f9d2c9
SimonRohou <simon.rohou@gmail.com>
2018-10-24 16:32:09 +0200

    [scripts] updated ibex-geom build

--- scripts/ibex/build_IbexGeometryLib.sh
+++ scripts/ibex/build_IbexGeometryLib.sh
@@ -9,7 +9,7 @@ git clone https://github.com/benEnsta/ibex-geometry
 cd ibex-geometry
 mkdir make
 cd make
-cmake -DWITH_PYTHON=OFF -DBUILD_TESTS=OFF -DIBEX_INCLUDE_DIR=/usr/local/include/ibex/ -DFILIB_INCLUDE_DIR=/usr/local/include/ibex/3rd/ -DPC_IBEX_LIBRARY_DIRS=/usr/local/lib/ibex/3rd/ ..
+cmake -DWITH_PYTHON=OFF -DBUILD_TESTS=OFF -DIBEX_INCLUDE_DIR=/usr/local/include/ibex/ -DFILIB_INCLUDE_DIR=/usr/local/include/ibex/3rd/ -DPC_IBEX_LIBRARY_DIRS=/usr/local/lib/ibex/3rd/ -DCMAKE_CXX_STANDARD=11 ..
 sudo make install
 cd ..
 cd ..
