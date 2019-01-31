d7c0b8492e9427c47c4928f09599d16d1e9dcb84
SimonRohou <simon.rohou@gmail.com>
2019-01-31 15:14:45 +0100

    [doc] technical documentation (DynamicalItem class)

--- src/core/dynamics/tubex_DynamicalItem.h
+++ src/core/dynamics/tubex_DynamicalItem.h
@@ -18,15 +18,56 @@
 
 namespace tubex
 {
+  /**
+   * \class DynamicalItem
+   * \brief Abstract class for common properties of Tube, TubeVector,
+   *        Slice, Trajectory, TrajectoryVector objects
+   */
+
   class DynamicalItem
   {
     public:
 
+      /**
+       * \brief Returns the dimension of the object
+       *
+       * \return n
+       */
       virtual int size() const = 0;
+
+      /**
+       * \brief Returns the temporal definition domain of this object
+       *
+       * \return an Interval object \f$[t_0,t_f]\f$
+       */
       virtual const ibex::Interval domain() const = 0;
+
+      /**
+       * \brief Returns the box of the feasible values along \f$[t_0,t_f]\f$
+       *
+       * \note Used for genericity purposes
+       *
+       * \return the envelope of codomain values
+       */
       virtual const ibex::IntervalVector codomain_box() const = 0;
+
+      /**
+       * \brief Returns the name of this class
+       *
+       * \note Only used for some generic display method
+       *
+       * \return the predefined name
+       */
       virtual const std::string class_name() const = 0;
 
+      /**
+       * \brief Verifies that this interval is a feasible temporal domain
+       *
+       * \note The domain must be non-empty, bounded and not degenerated
+       * \todo Allow unbounded domains such as \f$[t_0,\infty]\f$?
+       *
+       * \return true in case of valid temporal domain
+       */
       static bool valid_domain(const ibex::Interval& domain);
   };
 }
