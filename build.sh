9bb814a0d7af8a34665aed1fe2042104e7835e67
SimonRohou <simon.rohou@gmail.com>
2019-03-20 14:25:40 +0100

    [cmake] min version

--- build.sh
+++ build.sh
@@ -4,29 +4,47 @@
 
 #!/bin/bash
 
+# Possible commands:
+#  ./build.sh
+#  ./build.sh all
+#  ./build.sh tests
+#  ./build.sh examples
+#  ./build.sh clean
+#  ./build.sh all clean
+#  ./build.sh tests clean
+#  ./build.sh examples clean
+
 TUBEX_DIR="$(pwd)"
 
-if [ $# -ne 0 ] && ([ $1 = "clean" ] || [ $2 = "clean" ]) # cleaning before build
-then
-  find . -name make | xargs rm -fr
-fi
+# Cleaning before build
 
-mkdir make -p
-cd make
+  if [ $# -ne 0 ] && ([ $1 = "clean" ] || [ $2 = "clean" ])
+  then
+    find . -name make | xargs rm -fr
+  fi
 
-if [ $# -ne 0 ] && ([ $1 = "tests" ] || [ $1 = "all" ]) # build tests
-then
-  cmake -DBUILD_TESTS=ON ..
-else
-  cmake -DBUILD_TESTS=OFF ..
-fi
+# Building Tubex library
 
-make
-cd ..
+  mkdir make -p
+  cd make
 
-cd examples
-if [ $# -ne 0 ] && ([ $1 = "examples" ] || [ $1 = "all" ]) # build examples
-then
-  find . -name "ex\_*" | xargs -L 1 bash -c 'cd "$0" && ./build.sh && cd "$TUBEX_DIR"/'
-fi
-cd ..
+  # Possibly with tests
+
+    if [ $# -ne 0 ] && ([ $1 = "tests" ] || [ $1 = "all" ])
+    then
+      cmake -DBUILD_TESTS=ON ..
+    else
+      cmake -DBUILD_TESTS=OFF ..
+    fi
+
+  make
+  cd ..
+
+# Building examples independently
+
+  cd examples
+  if [ $# -ne 0 ] && ([ $1 = "examples" ] || [ $1 = "all" ])
+  then
+    find . -name "ex\_*" | xargs -L 1 bash -c 'cd "$0" && ./build.sh && cd "$TUBEX_DIR"/'
+  fi
+  cd ..
