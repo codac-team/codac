1c9bad33ee0cdf9c19546e654401f806b2804fc6
SimonRohou <simon.rohou@gmail.com>
2019-04-17 19:08:56 +0200

    [graphics] color maps

--- src/core/graphics/tubex_colors.h
+++ src/core/graphics/tubex_colors.h
@@ -63,6 +63,42 @@ namespace tubex
    * \return HSV value
    */
   hsv rgb2hsv(rgb rgb_value);
+
+  /**
+   * \brief Makes an RGV value from integers
+   *
+   * \param r red value, integer between 0 and 255
+   * \param g green value, integer between 0 and 255
+   * \param b blue value, integer between 0 and 255
+   * \return RGB value
+   */
+  rgb make_rgb(int r, int g, int b);
+
+  /**
+   * \brief Makes an RGV value from floats
+   *
+   * \param r red value, float between 0. and 1.
+   * \param g green value, float between 0. and 1.
+   * \param b blue value, float between 0. and 1.
+   * \return RGB value
+   */
+  rgb make_rgb(float r, float g, float b);
+
+  /**
+   * \brief Returns the RGB value of the Haxby color map corresponding to some ratio
+   *
+   * \param ratio a value between 0 and 1
+   * \return RGB value in Haxby color map
+   */
+  rgb haxby_color_map(float ratio);
+
+  /**
+   * \brief Returns the RGB value of the DEM color map corresponding to some ratio
+   *
+   * \param ratio a value between 0 and 1
+   * \return RGB value in DEM color map
+   */
+  rgb dem_color_map(float ratio);
 }
 
 #endif
