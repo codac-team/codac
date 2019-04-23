101537e162f767e1f88c53c18caf4d6bbdc4867c
SimonRohou <simon.rohou@gmail.com>
2019-04-23 09:52:04 +0200

    [examples] basics/01: minor update

--- examples/basics/ex_01_arithmetic/main.cpp
+++ examples/basics/ex_01_arithmetic/main.cpp
@@ -42,7 +42,7 @@ int main()
   /* =========== GRAPHICS =========== */
 
     vibes::beginDrawing();
-    VIBesFigTubeVector fig_x("x", 4, 4);
+    VIBesFigTubeVector fig_x("x", 0, 4);
     fig_x.set_properties(100, 100, 600, 300);
     fig_x.add_tubevector(&x, "x");
     fig_x.show();
