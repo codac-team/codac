ef06b592e5e828d7619f75be34bc6ab9b1c9694e
SimonRohou <simon.rohou@gmail.com>
2019-08-12 14:08:34 +0200

    [paving] ConnectedSubset doc

--- src/core/paving/tubex_ConnectedSubset.h
+++ src/core/paving/tubex_ConnectedSubset.h
@@ -85,8 +85,8 @@ namespace tubex
       /**
        * \brief Returns the boundary boxes of this subset
        *
-       * Note that the returned boxes are degenerated as
-       * they represent subpaving's edges.
+       * Note that the returned boxes are degenerated as they represent
+       * subpaving's edges.
        *
        * \param value_boundary value of the items representing the boundary of the subset
        * \param value_out value of the items representing no-solution boxes in the paving
@@ -98,14 +98,53 @@ namespace tubex
       /// \name Methods related to topological degree
       /// @{
 
+      /**
+       * \brief Tests if an unknown function \f$\mathbf{f}^*\f$ enclosed by 
+       *        \f$[\mathbf{f}]\f$ vanishes at least once on this subset
+       *
+       * This method stands on topological degree computations. This has been the object
+       * of the publication:
+       *
+       *     Proving the existence of loops in robot trajectories
+       *     S. Rohou, P. Franek, C. Aubry, L. Jaulin
+       *     The International Journal of Robotics Research, 2018
+       *
+       * \note If this method returns `false`, it does not mean a zero cannot exist on this
+       *       subset. It only corresponds to a case of undecidablility.
+       *
+       * \param f the inclusion function \f$[\mathbf{f}]:\mathbb{IR}^2\to\mathbb{IR}^2\f$
+       * \return `true` in case of at least one zero proven on this subset, `false` in case of undecidablility
+       */
       bool zero_proven(ibex::IntervalVector (*f)(const ibex::IntervalVector& b));
+
+      /**
+       * \brief Counts the number of zeros of an uncertain function \f$\mathbf{f}^*\f$
+       *        enclosed by \f$[\mathbf{f}]\f$ on this subset
+       *
+       * This method stands on topological degree computations. This has been the object
+       * of the publication:
+       *
+       *     Proving the existence of loops in robot trajectories
+       *     S. Rohou, P. Franek, C. Aubry, L. Jaulin
+       *     The International Journal of Robotics Research, 2018
+       *
+       * \param f the inclusion function \f$[\mathbf{f}]:\mathbb{IR}^2\to\mathbb{IR}^2\f$
+       * \param Jf Jacobian matrix \f$[\mathbf{J_f}]\f$ of the unknown function \f$\mathbf{f}^*\f$
+       * \param precision The subset may be made of wide boxes \f$[\mathbf{t}]_k\f$ that will
+       *                  result in an over-approximation of the \f$[\mathbf{J_f}]([\mathbf{t}]_k)\f$.
+       *                  A bisection of the \f$[\mathbf{t}]_k\f$ may be applied on order to deal with
+       *                  smaller boxes, thus reducing the pessimism of the Jacobian evaluation and thus
+       *                  the chances of concluding about the number of zeros. This parameter is the
+       *                  precision limit of this auto-refinement.
+       * \return the number of zeros, or -1 in case of undecidablility
+       */
       int zeros_number(ibex::IntervalVector (*f)(const ibex::IntervalVector& b), ibex::IntervalMatrix (*Jf)(const ibex::IntervalVector& b), float precision);
 
       /// @}
 
     protected:
 
-      /// \name Methods related to topological degree
+      /// \name Protected methods for topological degree computation
       /// @{
 
       int topological_degree(ibex::IntervalVector (*f)(const ibex::IntervalVector& b));
