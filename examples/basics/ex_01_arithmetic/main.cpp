f10b612cf45f5042678873f6468ff1e875bc4ac6
SimonRohou <simon.rohou@gmail.com>
2019-11-29 17:21:12 +0100

    [ex] restoring example 01

--- examples/basics/ex_01_arithmetic/main.cpp
+++ examples/basics/ex_01_arithmetic/main.cpp
@@ -1,38 +1,54 @@
+/** 
+ *  tubex-lib - Examples
+ *  Arithmetic on tubes: simple example
+ * ----------------------------------------------------------------------------
+ *
+ *  \brief      Example from the paper "Guaranteed Computation of Robot Trajectories"
+ *              Simon Rohou, Luc Jaulin, Lyudmila Mihaylova, Fabrice Le Bars, Sandor M. Veres
+ *
+ *  \date       2016
+ *  \author     Simon Rohou
+ *  \copyright  Copyright 2019 Simon Rohou
+ *  \license    This program is distributed under the terms of
+ *              the GNU Lesser General Public License (LGPL).
+ */
 
-  #include "tubex.h"
+#include "tubex.h"
+
+using namespace std;
+using namespace ibex;
+using namespace tubex;
+
+int main()
+{
+  /* =========== INITIALIZATION =========== */
   
-  using namespace std;
-  using namespace ibex;
-  using namespace tubex;
-  
-  int main()
-  {
-    // Trajectory from a formula
-    TrajectoryVector x(Interval(0.,10.),
-                       tubex::Function("(cos(t);sin(t))"));
+    Interval domain(0,10);
+    TubeVector x(domain, 6);
+    double timestep = 0.01;
 
-    // Discretized trajectory
-    TrajectoryVector y(2);
-    for(double t = 0. ; t <= 10. ; t+=0.6)
-    {
-      Vector val(2);
-      val[0] = cos(t); val[1] = sin(t);
-      y.set(val, t);
-    }
+    x[0] = Tube(domain, timestep, // tube [x](·)
+                tubex::Function("(t-5)^2 + [-0.5,0.5]"));
+    x[1] = Tube(domain, timestep, // tube [y](·)
+                tubex::Function("-4*cos(t-5) + [-0.5,0.5] + 0.1*(t-3.3)^2*[-2,2]"));
 
-    // Graphics:
+  /* =========== ARITHMETIC =========== */
+
+    x[2] = x[0] + x[1];      // tube [a](·)
+    x[3] = sin(x[0]);        // tube [b](·)
+    x[4] = x[0].primitive(); // tube [c](·)
+    x[5] = abs(x[1]);        // tube [d](·)
+
+  /* =========== GRAPHICS =========== */
+
     vibes::beginDrawing();
-    VIBesFigTube fig("Trajectories");
-    fig.set_properties(100, 100, 600, 300);
-
-    // From a formula
-    fig.add_trajectory(&y[0], "y_0", "gray"); // first component
-    fig.add_trajectory(&y[1], "y_1", "gray"); // second component
-
-    // From discrete values
-    fig.add_trajectory(&x[0], "x_0", "red");  // first component
-    fig.add_trajectory(&x[1], "x_1", "blue"); // second component
-
-    fig.show();
+    VIBesFigTubeVector fig_x("x", 0, 4);
+    fig_x.set_properties(100, 100, 600, 300);
+    fig_x.add_tubevector(&x, "x");
+    fig_x.show();
     vibes::endDrawing();
-  }
+    
+
+  // Checking if this example still works:
+  return (fabs(x.volume() - 233.765) < 1e-2) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
