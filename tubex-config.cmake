70e131be0e2253b47d178b3507c1eeecb2585dc8
SimonRohou <simon.rohou@gmail.com>
2020-05-07 12:31:08 +0200

    [ode] fixing tubex-config.cmake if ODE 3rd party not available

--- tubex-config.cmake
+++ tubex-config.cmake
@@ -15,15 +15,18 @@ find_library(TUBEX_ROB_LIBRARY NAMES tubex-rob
 find_library(TUBEX_PYIBEX_LIBRARY NAMES tubex-pyibex
              PATH_SUFFIXES lib)
 
-set(TUBEX_VERSION 2.5.0)
+set(TUBEX_VERSION 2.5.0) # todo: version number set in variable
 set(TUBEX_LIBRARIES ${TUBEX_LIBRARY} ${TUBEX_ROB_LIBRARY} ${TUBEX_PYIBEX_LIBRARY})
 set(TUBEX_INCLUDE_DIRS ${TUBEX_INCLUDE_DIR} ${TUBEX_ROB_INCLUDE_DIR} ${TUBEX_PYIBEX_INCLUDE_DIR})
 
-# Optional parts: ODE
+# Optional 3rd party:
+
+set(TUBEX_ODE_INCLUDE_DIR "") # optional
 find_path(TUBEX_ODE_INCLUDE_DIR tubex-ode.h
           PATH_SUFFIXES include/tubex-ode)
 set(TUBEX_INCLUDE_DIRS ${TUBEX_INCLUDE_DIRS} ${TUBEX_ODE_INCLUDE_DIR})
 
+set(TUBEX_ODE_LIBRARY "") # optional
 find_library(TUBEX_ODE_LIBRARY NAMES tubex-ode
              PATH_SUFFIXES lib)
 set(TUBEX_LIBRARIES ${TUBEX_LIBRARIES} ${TUBEX_ODE_LIBRARY})
