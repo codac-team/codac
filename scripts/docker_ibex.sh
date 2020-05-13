1799650e5fa4085481f38ccc7f7305d8d4ea1c71
SimonRohou <simon.rohou@gmail.com>
2020-05-13 14:53:11 +0200

    [docker] updated scripts

--- scripts/docker_ibex.sh
+++ scripts/docker_ibex.sh
@@ -7,7 +7,7 @@ if [ ! -d "/io/cache/ibex-lib" ]; then
   cd ibex-lib
   git checkout 47fc75a5e14eb2f353ae7e0b37ac0f9dad7801c0
   mkdir build && cd build
-  cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=/io/cache/ibex-lib/build_install ..
+  cmake -DCMAKE_CXX_FLAGS="-fPIC -msse3" -DCMAKE_C_FLAGS="-fPIC -msse3" -DCMAKE_INSTALL_PREFIX=/io/cache/ibex-lib/build_install ..
   make
   make install
 else
--- scripts/docker_tubex.sh
+++ scripts/docker_tubex.sh
@@ -4,7 +4,7 @@ git submodule init ; git submodule update # for pybind11 submodule
 mkdir build -p
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=/io/build_install -DCMAKE_PREFIX_PATH=/io/cache/ibex-lib/build_install -DBUILD_TESTS=ON -DWITH_TUBE_TREE=OFF -DWITH_CAPD=OFF -DPYTHON_VERSION="$py_version" ..
+cmake -DCMAKE_INSTALL_PREFIX=/io/tubex-lib/build_install -DCMAKE_PREFIX_PATH=/io/cache/ibex-lib/build_install -DBUILD_TESTS=ON -DWITH_TUBE_TREE=OFF -DWITH_CAPD=OFF -DPYTHON_VERSION="$py_version" ..
 make
 #make api
 #make doc
