4910471164aecbbaafc55213b49c4a427f6f364a
SimonRohou <simon.rohou@gmail.com>
2020-06-02 16:32:51 +0200

    [graphics] using drawAUV that is older and then more supported

--- src/robotics/graphics/tubex_VIBesFigMap.cpp
+++ src/robotics/graphics/tubex_VIBesFigMap.cpp
@@ -694,7 +694,8 @@ namespace tubex
     float robot_size = size == -1 ? m_robot_size : size;
     double robot_heading = pose.size() == 3 ? pose[2] : 0.;
     axis_limits(m_view_box | pose.subvector(0,1), true);
-    vibes::drawTank(pose[0], pose[1], robot_heading * 180. / M_PI, robot_size, "black[yellow]", params);
+    //vibes::drawTank(pose[0], pose[1], robot_heading * 180. / M_PI, robot_size, "black[yellow]", params);
+    vibes::drawAUV(pose[0], pose[1], robot_heading * 180. / M_PI, robot_size, "black[yellow]", params);
   }
 
   void VIBesFigMap::draw_vehicle(double t, const TrajectoryVector *traj, float size)
