633575fab58c43817531eb7255049e2cbba8de69
SimonRohou <simon.rohou@gmail.com>
2023-05-25 18:46:32 +0200

    [actions] python binding and tests

--- src/core/2/actions/codac2_Action.cpp
+++ src/core/2/actions/codac2_Action.cpp
@@ -16,6 +16,16 @@ using namespace codac;
 
 namespace codac2
 {
+  Interval sign(int a)
+  {
+    return (a > 0) ? 1 : ((a < 0) ? -1 : 0);
+  }
+
+  int isign(int a)
+  {
+    return (a > 0) ? 1 : ((a < 0) ? -1 : 0);
+  }
+
   OctaSym::OctaSym(const std::vector<int>& s) : std::vector<int>(s)
   {
     for(const auto& i : s)
@@ -35,14 +45,31 @@ namespace codac2
 
   OctaSym OctaSym::invert() const
   {
-    vector<int> inv(size());
+    OctaSym inv(*this);
     for(size_t i = 0 ; i < size() ; i++)
       inv[abs((int)(*this)[i])-1] = (abs((int)i)+1)*((*this)[i] >= 0 ? 1. : -1.);
-    return OctaSym(inv);
+    return inv;
   }
 
   CtcAction OctaSym::operator()(Ctc& ctc) const
   {
     return CtcAction(ctc, *this);
+  }
+
+  OctaSym operator*(const OctaSym& s1, const OctaSym& s2)
+  {
+    OctaSym s3(s1);
+    for(size_t i = 0 ; i < s3.size() ; i++)
+      s3[i] = isign(s2[i])*s1[abs((int)s2[i])-1];
+    return s3;
+  }
+  
+  std::ostream& operator<<(std::ostream& str, const OctaSym& s)
+  {
+    str << "(";
+    for(size_t i = 0 ; i < s.size() ; i++)
+      str << (i != 0 ? " " : "") << s[i];
+    str << ")" << flush;
+    return str;
   }
 }
