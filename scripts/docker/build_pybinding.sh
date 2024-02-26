caf1b909538920e255b5520695228c00f2e8c6c6
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2024-02-26 18:46:26 +0100

    Revert "Workaround for error "NumPy requires GCC >= 8.4" in CentOS docker"
    This reverts commit ee082308ac4cda3e5e7b69aab65136cbe15bf21c.

--- scripts/docker/build_pybinding.sh
+++ scripts/docker/build_pybinding.sh
@@ -30,7 +30,7 @@ for PYBIN in /opt/python/cp3*/bin; do
     auditwheel repair "$whl" -w /io/wheelhouse/
   done
   
-  "${PYBIN}/python" -m pip install "numpy<1.23" # Recent versions of numpy require GCC >= 8.4...
+  "${PYBIN}/python" -m pip install numpy
   "${PYBIN}/python" -m pip install codac --no-deps --no-index -f /io/wheelhouse
   (cd "$HOME"; "${PYBIN}/python" -m unittest discover codac.tests)
   cd /io
