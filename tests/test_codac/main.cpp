f1eb2ebe45a6efc4e9e7a7f800380e341f0ea983
Simon Rohou <simon.rohou@gmail.com>
2024-05-19 23:18:19 +0200

    [tests] added test_codac folder

---
+++ tests/test_codac/main.cpp
@@ -0,0 +1,18 @@
+#include <codac-core>
+
+using namespace codac2;
+
+// Download and run https://github.com/ENSTABretagneRobotics/VIBES/releases 
+// before to be able to see the tube in VIBes window...
+
+int main()
+{
+  VectorVar x(3);
+  AnalyticFunction f({x}, vec(
+    -sqr(x[2])+2*x[2]*sin(x[2]*x[0])+cos(x[2]*x[1]),
+    2*x[2]*cos(x[2]*x[0])-sin(x[2]*x[1])
+  ));
+
+  CtcInverse_<IntervalVector> ctc(f, IntervalVector({0.,0.}));
+  pave(IntervalVector({{0,2},{2,4},{0,10}}), ctc, 0.01);
+}
