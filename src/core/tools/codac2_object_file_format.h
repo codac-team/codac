60872d059c831fd7c3a48feeeeb4eab31a2ccf73
Simon Rohou <simon.rohou@gmail.com>
2024-05-22 11:46:00 +0200

    [tools] added export_to_ObjectFileFormat

---
+++ src/core/tools/codac2_object_file_format.h
@@ -0,0 +1,22 @@
+/** 
+ *  \file
+ *  OFF features
+ *  
+ * ----------------------------------------------------------------------------
+ *  \date       2024
+ *  \author     Simon Rohou
+ *  \copyright  Copyright 2024 Codac Team
+ *  \license    This program is distributed under the terms of
+ *              the GNU Lesser General Public License (LGPL).
+ */
+
+#pragma once
+
+#include <list>
+#include <string>
+#include "codac2_IntervalVector.h"
+
+namespace codac2
+{
+  void export_to_ObjectFileFormat(const std::list<IntervalVector>& l, const std::string& file_name, const std::string& color);
+}
