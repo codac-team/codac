84b29daaae16351ee6ebadc1d52af8846a8dd3cb
Simon Rohou <simon.rohou@gmail.com>
2024-06-13 15:05:58 +0200

    [ctc] added contractors for determinant of matrices and CtcSegment

---
+++ src/core/contractors/codac2_CtcSegment.h
@@ -0,0 +1,36 @@
+/** 
+ *  \file codac2_CtcSegment.h
+ * ----------------------------------------------------------------------------
+ *  \date       2024
+ *  \author     Simon Rohou
+ *  \copyright  Copyright 2024 Codac Team
+ *  \license    GNU Lesser General Public License (LGPL)
+ */
+
+#pragma once
+
+#include "codac2_Ctc.h"
+#include "codac2_IntervalVector.h"
+
+namespace codac2
+{
+  class CtcSegment : public Ctc_<IntervalVector>
+  {
+    public:
+
+      /*explicit CtcSegment(const Vector& a, const Vector& b)
+        : CtcSegment(IntervalVector(a), IntervalVector(b))
+      { }
+
+      explicit */CtcSegment(const IntervalVector& a, const IntervalVector& b)
+        : Ctc_<IntervalVector>(2), _a(a), _b(b)
+      { }
+
+      std::shared_ptr<Ctc> copy() const;
+      void contract(IntervalVector& x) const;
+
+    protected:
+
+      const IntervalVector _a, _b;
+  };
+}
--- src/core/domains/interval/codac2_IntervalMatrix.h
+++ src/core/domains/interval/codac2_IntervalMatrix.h
@@ -99,8 +99,8 @@ namespace codac2
         : IntervalMatrixTemplate_<IntervalMatrix,codac2::Matrix>(l)
       { }
 
-      // too ambiguous (IntervalVector can only be row): // Defined in codac2_IntervalVector.h
-      // too ambiguous (IntervalVector can only be row): explicit IntervalMatrix(std::initializer_list<IntervalVector> l);
+      // Defined in codac2_IntervalVector.h
+      explicit IntervalMatrix(std::initializer_list<IntervalVector> l);
 
       IntervalMatrix(const Matrix& x)
         : IntervalMatrixTemplate_<IntervalMatrix,codac2::Matrix>(x)
