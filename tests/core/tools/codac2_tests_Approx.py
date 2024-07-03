acb59125ff2bd62316482dc9d304557edca125ad
Simon Rohou <simon.rohou@gmail.com>
2024-07-03 18:51:37 +0200

    [tests] Approx

--- tests/core/tools/codac2_tests_Approx.py
+++ tests/core/tools/codac2_tests_Approx.py
@@ -17,6 +17,11 @@ class TestApprox(unittest.TestCase):
 
     self.assertFalse(Interval(0.1) == Interval(1.)/Interval(10.))
     self.assertTrue(Approx(Interval(0.1)) == Interval(1.)/Interval(10.))
+    self.assertTrue(Interval(0.1) == Approx(Interval(1.)/Interval(10.)))
+
+    #self.assertTrue(IntervalVector([[0.1]]) != IntervalVector([1.])/Interval(10.))
+    #self.assertTrue(Approx(IntervalVector([[0.1]])) == IntervalVector([1.])/Interval(10.))
+    #self.assertTrue(IntervalVector([[0.1]]) == Approx(IntervalVector([1.])/Interval(10.)))
       
 if __name__ ==  '__main__':
   unittest.main()
