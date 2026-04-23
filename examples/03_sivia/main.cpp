74fc70e90e1a23211a19df70366df58c0468469b
godardma <mael.godard@ensta-bretagne.org>
2026-04-23 10:14:11 +0200

    [pave] paving_multithread v2

--- examples/03_sivia/main.cpp
+++ examples/03_sivia/main.cpp
@@ -4,10 +4,8 @@ using namespace codac2;
 
 int main()
 {
-  set_nb_threads(max_threads());
-
   VectorVar x(2);
   AnalyticFunction f { {x}, sqr(x[0])*sin(sqr(x[0])+sqr(x[1]))-sqr(x[1]) };
-  auto p = pave_multithread({{-5,5},{-4,4}}, f, {0,oo}, 1e-1, true);
+  auto p = sivia({{-5,5},{-4,4}}, f, {0,oo}, 1e-2, true);
   DefaultFigure::draw_paving(p);
 }
