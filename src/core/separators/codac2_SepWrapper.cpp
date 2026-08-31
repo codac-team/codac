96c248181e06f2d0d9774a91f963a40051c7921e
Simon Rohou <simon.rohou@gmail.com>
2026-08-31 15:49:37 +0200

    [pav] corrected bug in PavingOut::operator&

--- src/core/separators/codac2_SepWrapper.cpp
+++ src/core/separators/codac2_SepWrapper.cpp
@@ -29,12 +29,11 @@ BoxPair SepWrapper<PavingInOut>::separate(const IntervalVector& x) const
 
   // The parts of the box that are outside of the paving can not be classified
   auto d = x.diff(_P.tree()->hull());
-  for (const auto& di : d)
+  for(const auto& di : d)
   {
     result.inner |= di;
     result.outer |= di;
   }
-
 
   // Inner computation
   for(const auto &box: _P.boxes(PavingInOut::outer_complem, x))
