0d922e024cd5d895b031e22d45188cf7e87098b3
Jordan08 <jordan.ninin@gmail.com>
2026-09-19 13:37:35 +0200

    Use the portable PI constant instead of the non-standard M_PI

--- tests/extensions/capd/codac2_tests_peibos_capd.cpp
+++ tests/extensions/capd/codac2_tests_peibos_capd.cpp
@@ -9,6 +9,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <codac2_AnalyticFunction.h>
+#include <codac2_math.h>
 #include <codac2_peibos_capd.h>
 
 using namespace std;
@@ -20,7 +21,7 @@ TEST_CASE("PEIBOS_CAPD")
     capd::IMap vectorField_pend("var:t,w;fun:w,-sin(t) - 0.5*w;");
     VectorVar X(1);
     AnalyticFunction psi0_pend ({X},{0.1*X[0],0.1});
-    auto peibos_output_pend = PEIBOS(vectorField_pend, 1.0, 0.5, psi0_pend, {OctaSym({1,2})}, 0.2, {-M_PI/2.,0.});
+    auto peibos_output_pend = PEIBOS(vectorField_pend, 1.0, 0.5, psi0_pend, {OctaSym({1,2})}, 0.2, {-PI/2.,0.});
     auto m_v_par_2d_pend = reach_set(peibos_output_pend);
   }
 }
