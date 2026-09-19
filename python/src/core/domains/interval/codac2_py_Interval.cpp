8fd99f768a6189efc34c9f0c5d1ed426aedc1a74
Jordan08 <jordan.ninin@gmail.com>
2026-09-19 13:37:36 +0200

    Disable Clang's -Wself-assign-overloaded in in-place operator bindings

--- python/src/core/domains/interval/codac2_py_Interval.cpp
+++ python/src/core/domains/interval/codac2_py_Interval.cpp
@@ -20,6 +20,12 @@
 #include "codac2_py_matlab.h"
 #include "codac2_py_deprecated.h"
 
+// pybind11 binds the in-place operators as py::self |= py::self and the like,
+// which Clang reads as a variable assigned to itself (-Wself-assign-overloaded)
+#if defined(__clang__)
+  #pragma clang diagnostic ignored "-Wself-assign-overloaded"
+#endif
+
 using namespace std;
 using namespace codac2;
 namespace py = pybind11;
