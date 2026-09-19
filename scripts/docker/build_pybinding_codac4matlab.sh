b17a741816d73fc2f7465b508bbc386a2b2c88f4
Jordan08 <jordan.ninin@gmail.com>
2026-09-19 05:30:39 +0200

    Stop installing IBEX in the workflows, Docker scripts and packages

--- scripts/docker/build_pybinding_codac4matlab.sh
+++ scripts/docker/build_pybinding_codac4matlab.sh
@@ -2,11 +2,6 @@
 
 set -e -x
 
-wget https://github.com/lebarsfa/ibex-lib/releases/download/ibex-2.8.9.20260819/ibex_$(uname -m)_manylinux_2_28.zip --no-check-certificate -nv
-unzip -q ibex_$(uname -m)_manylinux_2_28.zip
-rm -Rf ibex_$(uname -m)_manylinux_2_28.zip
-sudo cp -Rf ibex/* /usr/local/
-
 git config --global --add safe.directory /io
 cd /io
 
